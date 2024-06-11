764ce3f042b1179bd056d5211204c23359663fee
Eric Kuha <erickuha@gmail.com>
2024-06-11 09:19:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ for file in $files; do
     echo "Creating symlink to $file in ~"
     ln -s $dir/$file ~/.$file
 done
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
